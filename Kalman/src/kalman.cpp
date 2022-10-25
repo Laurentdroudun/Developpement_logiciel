6362feb7b111806510a6511540184987b0ec3f12
Laurent Droudun <laurent.droudun@gmail.com>
2022-10-25 17:24:58 +0200

    add camera subscriber

--- Kalman/src/kalman.cpp
+++ Kalman/src/kalman.cpp
@@ -2,14 +2,12 @@
 #include <rclcpp/rclcpp.hpp>
 
 
-using namespace std;
-
-class quad_sub : public rclpp:Node
+class quad_sub : public rclcpp:Node
 {
   public :
   quad_sub() : Node("quad_sub")
   {
-    sub=this->create_subscription<quad_msg_type>("topic",10,std::bind(&quad_sub::quad_callback,this,_1));
+    sub=this->create_subscription<quad_msg_type>("topic_quad",10,std::bind(&quad_sub::quad_callback,this,_1));
   }
   private :
   {
@@ -28,4 +26,20 @@ int main(int argc, char ** argv)
   rclcpp::spin(std::make_shared<quad_sub>());
   rclcpp::shutdown();
   return 0;
+}
+
+class cam_sub : public rclcpp:Node
+{
+  public :
+  cam_sub() : Node("cam_sub")
+  {
+    sub=this->create_subscription<cam_msg_type>("topic_cam",10,std::bind(&cam_sub::cam_callback,this,_1));
+  }
+  private :
+  {
+    void cam_callback(const cam_msg_type &msg) const {
+      RCLCPP_INFO(this->get_logger(),"I heard: '%s'",msg.data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub;
+  }
 }
